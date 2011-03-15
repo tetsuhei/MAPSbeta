c9097d00126ead6e57555c23d12dffa3890bba42
Tetsu Nakashima <btn@ualberta.ca>
2011-03-15 14:05:42 -0600

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MAPSbeta::Application.config.secret_token = '4dab69d29f356782418234367784849032a33d10dd5e891d621f5cc5a187416b7ce0788420ca468a9efab59eb6c2bb161ee637b1124156eb1f8319120abab82e'
